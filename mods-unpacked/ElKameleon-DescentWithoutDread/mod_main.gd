234b47bb4d1354aae4b91b36dc59e4c18eae93a2
ElKameleon <el.kameleon@gmail.com>
2026-04-18 09:16:18 -0500

    v2.1 - Migrate to GML, self-contained waypoint manager, main menu marker

---
+++ mods-unpacked/ElKameleon-DescentWithoutDread/mod_main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const ELKAMELEON_DESCENTWITHOUTDREAD_DIR := "ElKameleon-DescentWithoutDread"
+const ELKAMELEON_DESCENTWITHOUTDREAD_LOG_NAME := "ElKameleon-DescentWithoutDread:Main"
+
+var mod_dir_path := ""
+
+func _init() -> void:
+    mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(ELKAMELEON_DESCENTWITHOUTDREAD_DIR)
+
+func _ready() -> void:
+    var waypoint_manager = load(mod_dir_path.path_join("scripts/waypoint_manager.gd")).new()
+    waypoint_manager.MOD_DIR = mod_dir_path
+    get_tree().root.call_deferred("add_child", waypoint_manager)
--- mods-unpacked/ElKameleon-DescentWithoutDread/scripts/waypoint_manager.gd
+++ mods-unpacked/ElKameleon-DescentWithoutDread/scripts/waypoint_manager.gd
@@ -1,12 +1,21 @@
 extends Node
 const WAYPOINTS_FILE_PATH: String = "user://waypoints.cfg"
+var MOD_DIR: String
+
 var config: ConfigFile
 var markers: Dictionary = {}
+var _markers_spawned_for_scene: String = ""
+var _current_scene_path: String = ""
+
+var menu_marker: Node3D = null
+const MENU_MARKER_POSITION := Vector3(86.12, 5.952, 51.66)
 
 func _init():
     pass
 
 func _ready():
+    print("waypoint_manager _ready called")
+    set_process_unhandled_input(true)
     config = ConfigFile.new()
     config.load(WAYPOINTS_FILE_PATH)
     var scenes = ["FogLands", "FogLands_Invert", "ViperPit", "first_kiln_invert"]
@@ -16,9 +25,59 @@ func _ready():
                 config.set_value(scene, str(i), "")
     config.save(WAYPOINTS_FILE_PATH)
 
+func _process(_delta: float) -> void:
+    print("process running")
+    var scene = get_tree().current_scene
+    if scene == null:
+        return
+
+    var path = scene.scene_file_path
+    var scene_changed = path != _current_scene_path
+    if scene_changed:
+        _current_scene_path = path
+
+    if is_main_menu_scene():
+        clear_spawned_markers()
+        spawn_main_menu_marker()
+        return
+    else:
+        clear_main_menu_marker()
+
+    if not is_valid_scene():
+        clear_spawned_markers()
+        return
+
+    if Game.climber == null:
+        return
+
+    var scene_key = get_scene_key()
+    if scene_changed or _markers_spawned_for_scene != scene_key:
+        _markers_spawned_for_scene = scene_key
+        initialize_scene(scene_key)
+        spawn_existing_markers()
+
+func _unhandled_input(event: InputEvent) -> void:
+    if not is_valid_scene():
+        return
+    if Game.climber == null:
+        return
+    if event is InputEventKey and event.pressed and not event.echo:
+        for i in range(1, 10):
+            if event.keycode == KEY_1 + (i - 1):
+                if event.ctrl_pressed:
+                    save_waypoint(i, Game.climber.global_position)
+                else:
+                    teleport_to_waypoint(i)
+        if event.keycode == KEY_BACKSPACE and event.ctrl_pressed:
+            clear_waypoints()
+
 func is_valid_scene() -> bool:
     var path = get_tree().current_scene.scene_file_path
     return path.contains("FogLands") or path.contains("ViperPit") or path.contains("first_kiln_invert")
+
+func is_main_menu_scene() -> bool:
+    var scene = get_tree().current_scene
+    return scene != null and scene.scene_file_path.contains("MainMenu")
 
 func get_scene_key() -> String:
     return get_tree().current_scene.scene_file_path.get_file().get_basename()
@@ -64,10 +123,7 @@ func clear_waypoints() -> void:
     for i in range(1, 10):
         config.set_value(scene_key, str(i), "")
     config.save(WAYPOINTS_FILE_PATH)
-    for marker in markers.values():
-        if is_instance_valid(marker):
-            marker.queue_free()
-    markers.clear()
+    clear_spawned_markers()
     display_message("All waypoints cleared!")
 
 func teleport_to_waypoint(slot: int) -> void:
@@ -90,7 +146,7 @@ func teleport_to_waypoint(slot: int) -> void:
 func spawn_marker(slot: int, position: Vector3) -> void:
     if markers.has(slot) and is_instance_valid(markers[slot]):
         markers[slot].queue_free()
-    var marker = preload("res://scenes/waypoint_marker.tscn").instantiate()
+    var marker = load(MOD_DIR + "/scenes/waypoint_marker.tscn").instantiate()
     var scene = get_tree().current_scene
     scene.add_child(marker)
     marker.global_position = position
@@ -98,9 +154,30 @@ func spawn_marker(slot: int, position: Vector3) -> void:
 
 func spawn_existing_markers() -> void:
     config.load(WAYPOINTS_FILE_PATH)
-    markers.clear()
+    clear_spawned_markers()
     var scene_key = get_scene_key()
     for i in range(1, 10):
         var position = config.get_value(scene_key, str(i), null)
         if typeof(position) == TYPE_VECTOR3:
             spawn_marker(i, position)
+
+func clear_spawned_markers() -> void:
+    for marker in markers.values():
+        if is_instance_valid(marker):
+            marker.queue_free()
+    markers.clear()
+    _markers_spawned_for_scene = ""
+
+func spawn_main_menu_marker() -> void:
+    if is_instance_valid(menu_marker):
+        return
+    var marker = load(MOD_DIR + "/scenes/waypoint_marker.tscn").instantiate()
+    var scene = get_tree().current_scene
+    scene.add_child(marker)
+    marker.position = MENU_MARKER_POSITION
+    menu_marker = marker
+
+func clear_main_menu_marker() -> void:
+    if is_instance_valid(menu_marker):
+        menu_marker.queue_free()
+    menu_marker = null
